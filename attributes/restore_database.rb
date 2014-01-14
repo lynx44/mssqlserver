aa36c81efcd1c29b6c39845c98d2ebc70e41aa6a
Matt Clifton <spartacus003@hotmail.com>
2014-01-14 10:47:03 -0800

    Updated readme

--- attributes/restore_database.rb
+++ attributes/restore_database.rb
@@ -1,7 +1,7 @@
 default['mssqlserver']['restore']['filepath'] = nil #required
 default['mssqlserver']['restore']['data_directory'] = 'C:\Program Files\Microsoft SQL Server\MSSQL11.MSSQLSERVER\MSSQL\DATA'
 default['mssqlserver']['restore']['database'] = nil #required
-default['mssqlserver']['restore']['server'] = nil
+#default['mssqlserver']['restore']['server'] = nil
 default['mssqlserver']['restore']['username'] = nil
 default['mssqlserver']['restore']['password'] = nil
 default['mssqlserver']['restore']['instance'] = 'localhost'
