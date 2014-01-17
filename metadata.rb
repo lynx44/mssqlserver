27fd1791e17045f13d180cfe0a530c3905f1df3a
Matt Clifton <spartacus003@hotmail.com>
2014-01-16 21:45:38 -0800

    Removed dependencies, fixed attribute issue
    Fixed default attribute issue in restore_database. Removed unneeded
    dependencies. Updated specs to have better description.

--- metadata.rb
+++ metadata.rb
@@ -7,6 +7,4 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
 depends			 'windows'
 depends      'powershell'
-depends      'windows_configuration'
-depends      'dotnetframework'
 depends      'windows_firewall'
