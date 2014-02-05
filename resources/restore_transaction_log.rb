e8387893d1be58e7ca0e9b045ab0537a3e9692af
Matt Clifton <mattc@geocaching.com>
2014-02-04 17:30:00 -0800

    added backup and restore lwrps for database and transaction logs'

--- resources/restore_transaction_log.rb
+++ resources/restore_transaction_log.rb
@@ -1,6 +1,6 @@
 actions :run
 default_action :run
 
-attribute :destination, :kind_of => String
+attribute :source, :kind_of => String
 attribute :database, :kind_of => String
 attribute :with, :kind_of => Array
