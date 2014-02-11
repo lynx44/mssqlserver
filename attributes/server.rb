322de9e161cb5086780e160aab167bee82ec4bf1
Matt Clifton <spartacus003@hotmail.com>
2014-02-10 23:52:08 -0800

    updated server attributes with instructions

---
+++ attributes/server.rb
@@ -0,0 +1,5 @@
+default['mssqlserver']['server']['url'] = nil #url to zip file. download the iso, extract the contents to a folder, then zip it.
+default['mssqlserver']['server']['checksum'] = "0f70016eeb48a823d03be3e9e70019f1f5279ef22037d5726e362b923caf6c06"
+default['mssqlserver']['server']['package_name'] = "Microsoft SQL Server 2012 (64-bit)"
+default['mssqlserver']['server']['sa_password'] = nil
+default['mssqlserver']['server']['open_firewall_port'] = false
