1a7f3fe1689b739b5100b4c1a1251828389865d2
mattc <mattc@geocaching.com>
2014-09-30 14:51:59 -0700

    changed database swap provider to use resource attributes

--- providers/database_swap.rb
+++ providers/database_swap.rb
@@ -1,40 +1,42 @@
 action :run do
-  source_kill_command = MsSqlServer::RenameScripts.create_kill_script(node['source_name'])
+  source = @new_resource.source
+  destination = @new_resource.destination
+  source_kill_command = MsSqlServer::RenameScripts.create_kill_script(source)
 
-  mssqlserver_sql_command "kill connections to #{node['source_name']}" do
+  mssqlserver_sql_command "kill connections to #{source}" do
     command source_kill_command
     database 'master'
     action :run
   end
 
-  dest_kill_command = MsSqlServer::RenameScripts.create_kill_script(node['dest_name'])
+  dest_kill_command = MsSqlServer::RenameScripts.create_kill_script(destination)
 
-  mssqlserver_sql_command "kill connections to #{node['dest_name']}" do
+  mssqlserver_sql_command "kill connections to #{destination}" do
     command dest_kill_command
     database 'master'
     action :run
   end
 
-  destination_temp_name = "#{node['dest_name']}_Renamed"
-  destination_temp_rename_command = MsSqlServer::RenameScripts.create_rename_script(node['dest_name'], destination_temp_name)
+  destination_temp_name = "#{destination}_Renamed"
+  destination_temp_rename_command = MsSqlServer::RenameScripts.create_rename_script(destination, destination_temp_name)
 
-  mssqlserver_sql_command "rename #{node['dest_name']} to #{destination_temp_name}" do
+  mssqlserver_sql_command "rename #{destination} to #{destination_temp_name}" do
     command destination_temp_rename_command
     database 'master'
     action :run
   end
 
-  source_rename_command = MsSqlServer::RenameScripts.create_rename_script(node['source_name'], node['dest_name'])
+  source_rename_command = MsSqlServer::RenameScripts.create_rename_script(source, destination)
 
-  mssqlserver_sql_command "rename #{node['source_name']} to #{node['dest_name']}" do
+  mssqlserver_sql_command "rename #{source} to #{destination}" do
     command source_rename_command
     database 'master'
     action :run
   end
 
-  dest_rename_command = MsSqlServer::RenameScripts.create_rename_script(destination_temp_name, node['source_name'])
+  dest_rename_command = MsSqlServer::RenameScripts.create_rename_script(destination_temp_name, source)
 
-  mssqlserver_sql_command "rename #{destination_temp_name} to #{node['source_name']}" do
+  mssqlserver_sql_command "rename #{destination_temp_name} to #{source}" do
     command dest_rename_command
     database 'master'
     action :run
